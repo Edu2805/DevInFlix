448a3dda68c22e515848e18e3250ff6ee82fae2e
Eduardo <eduardoamorim927@gmail.com>
2022-01-20 10:46:16 -0300

    Foi adicionado o README.md

---
+++ README.md
@@ -0,0 +1,7 @@
+Projeto DEVINFLIX
+
+Projeto sendo realizado com estudantes do curso de desenvolvimento de sistemas
+em Java/JavaScript/Spring/SQL do DevInHouse em parceria com o SENAI Santa
+Catarina, Acate e Senior Sistemas.
+
+O projeto consiste em simular uma plataforma de filmes
--- src/menus/ShowMenu.java
+++ src/menus/ShowMenu.java
@@ -18,7 +18,7 @@ import interatividades.Interacao;
 import usuarios.PessoaFisica;
 
 public class ShowMenu implements FuncoesDoMenu {
-	
+
 	@Override
 	public void menuUsuario() {
 		Scanner sc = new Scanner(System.in);
@@ -26,7 +26,7 @@ public class ShowMenu implements FuncoesDoMenu {
 		List<PessoaFisica> cadastroUsuarios = new ArrayList<>();
 		GenerosMaisAssistidosUsuarios generosMaisUsuarios = new GenerosMaisAssistidosUsuarios();
 		GenerosMaisAssistidosPlataforma generosMaisPlataforma = new GenerosMaisAssistidosPlataforma();
-		
+
 		System.out.println("########################## DEVINFLIX ##########################");
 		System.out.println("\n################## FILMES E SÉRIES EM CARTAZ ##################\n");
 
@@ -57,7 +57,6 @@ public class ShowMenu implements FuncoesDoMenu {
 		listaDeFilmes.add(crepusculo);
 		listaDeFilmes.add(harryPotter);
 
-		// Exibe a listaDeFilmes
 		for (Filme filme : listaDeFilmes) {
 			System.out.println(filme.getNome());
 		}
@@ -75,7 +74,7 @@ public class ShowMenu implements FuncoesDoMenu {
 		int secaoUsuario = 0;
 		int controlaListaUsuarios = 0;
 
-		
+		// Variáveis de controle dos generos mais assistidos
 		int acaoUsuario;
 		int aventuraUsuario;
 		int romanceUsuario;
@@ -85,7 +84,7 @@ public class ShowMenu implements FuncoesDoMenu {
 		int independentesUsuario;
 		int suspenseUsuario;
 		int outrosUsuario;
-		
+
 		int acaoPlataforma;
 		int aventuraPlataforma;
 		int romancePlataforma;
@@ -95,7 +94,9 @@ public class ShowMenu implements FuncoesDoMenu {
 		int independentesPlataforma;
 		int suspensePlataforma;
 		int outrosPlataforma;
-		
+
+		boolean pagamento = false;
+
 		while (entrarMenu != 2) {
 
 			String nome;
@@ -150,205 +151,247 @@ public class ShowMenu implements FuncoesDoMenu {
 
 			cadastroUsuarios.add(listaDeUsuarios);
 
-			System.out.println("Seja bem-vindo ao DevInFlix, abaixo você pode conferir nosso catálogo de filmes\n");
-
-			int indiceFilme = 0;
-			for (Filme filme : listaDeFilmes) {
-				String nomeFilme = filme.getNome();
-				String sinopse = filme.getSinopse();
-				String generoFilme = filme.getGenero();
-				indiceFilme++;
-
-				System.out.println("OPÇÃO " + (indiceFilme) + "- GENERO: " + generoFilme + ", FILME: " + nomeFilme
-						+ ", SINÓPSE: " + sinopse);
-			}
-			System.out.println(
-					"--------------------------------------------------------------------------------------------------");
-			
-			
-			//Abre lista para escolha do filme
-			List<Filme> escolhaFilmesDaLista = new ArrayList<>();
-			
-			escolhaFilmesDaLista.add(exterminadorDoFuturo);
-			escolhaFilmesDaLista.add(clubeDaLuta);
-			escolhaFilmesDaLista.add(oLoboDeWallStreet);
-			escolhaFilmesDaLista.add(ameliePoulain);
-			escolhaFilmesDaLista.add(crepusculo);
-			escolhaFilmesDaLista.add(harryPotter);
-			
-			boolean stopWhileNovoFilme = false;
-			int escolhaFilme = 0;
-			while(!stopWhileNovoFilme) {
-				System.out.print("Olá " + listaDeUsuarios.getNomeCompleto() + ", escolha um dos filmes para você assistir: ");
-				escolhaFilme = sc.nextInt() - 1;
-				sc.nextLine();
-				listaDeUsuarios.escolhaDoFilme(escolhaFilmesDaLista, escolhaFilme);
-				generosMaisUsuarios.generosMaisAssistidosUsuario(escolhaFilmesDaLista, cadastroUsuarios, escolhaFilme, secaoUsuario);
-				generosMaisPlataforma.generosMaisAssistidosPlataforma(escolhaFilmesDaLista, cadastroUsuarios, escolhaFilme, secaoUsuario);
-				
-				System.out.println("\nDeseja escolher mais algum filme?1- SIM, 2- NÃO");
+			boolean stopWhileMenuPagamento = true;
+			while (stopWhileMenuPagamento) {
+				System.out.println("Escolha a forma de pagamento\n1- Cartão de crédito\n2- PIX\n3- Boleto\n4- Sair");
 				System.out.print("-->");
-				int maisFilmes = sc.nextInt();
+				int formaPagamento = sc.nextInt();
 
-				if (maisFilmes == 1) {
-					stopWhileNovoFilme = false;
-				} else if (maisFilmes == 2) {
-					stopWhileNovoFilme = true;;
-				} else {
-					System.out.println("\nPor gentileza, digite uma opção válida.\n");
-				}
-			}
-		
-			//Método curtir/Descurtit
-			listaDeUsuarios.likeDeslike();
-			
-			// Exibe a lista de usuários para que o usuário da secao possa escoher outros
-			// usuário para indicar filmes
-			int escolha = 0;
-			boolean usuarioRecomenda = true;
-			boolean stopWhileRecomenda = true;
-			int controleEnunciado = secaoUsuario;
+				switch (formaPagamento) {
+				case 1:
+					System.out.println("Você irá pagar com cartão de crédito");
+					pagamento = true;
+					stopWhileMenuPagamento = false;
+					break;
 
-			int sair = 0;
+				case 2:
+					System.out.println("Você irá pagar com PIX");
+					pagamento = true;
+					stopWhileMenuPagamento = false;
+					break;
 
-			while (usuarioRecomenda) {
-				while (stopWhileRecomenda) {
-					int controleLista = 0;
+				case 3:
+					System.out.println("Você irá pagar com Boleto");
+					pagamento = true;
+					stopWhileMenuPagamento = false;
+					break;
+					
+				case 4:
+					pagamento = false;
+					stopWhileMenuPagamento = false;
+					break;
 
-					for (int i = controlaListaUsuarios; i < cadastroUsuarios.size(); i++) {
-						if (secaoUsuario == i) {
-							/**
-							 * Impede que o usuário selecione ele mesmo para recomendar filmes, permite que
-							 * o indice dele nao seja exibido enquanto usa o sistema
-							 */
-
-						} else {
-
-							if (controleEnunciado == secaoUsuario) {
-
-								System.out.println(
-										"----------------------------------------------------------------------------\n");
-								System.out.println(
-										"Agora precisamos que você selecione um dos usuários listados abaixo e faça uma recomendação de filme\n");
-								System.out.println("Lembrando que você só pode fazer uma recomendação por mês :)");
-							}
-							controleEnunciado++;
-
-							String nomeUsuarioLista = cadastroUsuarios.get(i).getNomeCompleto();
-							System.out.println((i + 1) + "- " + nomeUsuarioLista);
-							controleLista = 2;
-						}
-					}
-
-					// Fazer um loop com excecoes
-					if (controleLista == 2) {
-						System.out.print("-->");
-						escolha = sc.nextInt();
-
-						for (int h = 0; h < cadastroUsuarios.size(); h++) {
-							if (escolha <= cadastroUsuarios.size()) {
-								System.out.println(
-										"Você escolheu " + cadastroUsuarios.get(escolha - 1).getNomeCompleto());
-								System.out.println(
-										"----------------------------------------------------------------------------");
-								// Método para Recomendacoes de filmes
-								listaDeUsuarios.usuarioRecomenda();
-
-								stopWhileRecomenda = false;
-								break;
-							} else {
-								System.out.println(
-										"\nPor gentileza, digite um número correspondente a lista de usuários.\n");
-							}
-							break;
-						}
-					}
-					stopWhileRecomenda = false;
-				}
-				sair++;
-				if (sair == cadastroUsuarios.size()) {
+				default:
+					System.out.println("\nForma de pagamento inválida\n");
+					pagamento = false;
 					break;
 				}
 			}
 
-			//Sugestão de filme usuário
-			Interacao sugestaoFilmeUsuarios = new Interacao();
-			sugestaoFilmeUsuarios.sugestaoDoUuario();
+			if (pagamento == true) {
 
-			// Criar um método para armazenar a recomendacao
+				System.out.println("Seja bem-vindo ao DevInFlix, abaixo você pode conferir nosso catálogo de filmes\n");
 
-			
-			
-			
-			
-			
-			acaoUsuario = generosMaisUsuarios.getContAcao();
-			aventuraUsuario = generosMaisUsuarios.getContAventura();
-			romanceUsuario = generosMaisUsuarios.getContRomance();
-			cartoonUsuario = generosMaisUsuarios.getContCartoon();
-			dramaUsuario = generosMaisUsuarios.getContDrama();
-			documentarioUsuario = generosMaisUsuarios.getContDocumentario();
-			independentesUsuario = generosMaisUsuarios.getContIndependentes();
-			suspenseUsuario = generosMaisUsuarios.getContSuspense();
-			outrosUsuario = generosMaisUsuarios.getContOutros();
-			
-			acaoPlataforma = generosMaisPlataforma.getContAcao();
-			aventuraPlataforma = generosMaisPlataforma.getContAventura();
-			romancePlataforma = generosMaisPlataforma.getContRomance();
-			cartoonPlataforma = generosMaisPlataforma.getContCartoon();
-			dramaPlataforma = generosMaisPlataforma.getContDrama();
-			documentarioPlataforma = generosMaisPlataforma.getContDocumentario();
-			independentesPlataforma = generosMaisPlataforma.getContIndependentes();
-			suspensePlataforma = generosMaisPlataforma.getContSuspense();
-			outrosPlataforma = generosMaisPlataforma.getContOutros();
-			
-			SomaGenerosUsuarios comparaGenerosMaisUsuarios = new SomaGenerosUsuarios(acaoUsuario, aventuraUsuario, romanceUsuario, cartoonUsuario, dramaUsuario, documentarioUsuario, independentesUsuario, suspenseUsuario, outrosUsuario);
-			SomaGenerosPlataforma comparaGenerosMaisPlataforma = new SomaGenerosPlataforma(acaoPlataforma, aventuraPlataforma, romancePlataforma, cartoonPlataforma, dramaPlataforma, documentarioPlataforma, independentesPlataforma, suspensePlataforma, outrosPlataforma);
-			
-			//Generos mais assistidos plataforma
-			
-			
-			System.out.println("Os generos mais assistidos por " + listaDeUsuarios.getNomeCompleto() + " são:");
-			comparaGenerosMaisUsuarios.generoMaisUsuario();
-			
-			System.out.println("\nOs generos mais assistidos do DevInFlix são:");
-			comparaGenerosMaisPlataforma.generoMaisPlataforma();
-			
-		
-			boolean saidaPrograma = true;
-			while (saidaPrograma) {
-				boolean stopWhile = true;
-				while (stopWhile) {
-					System.out.println("\nDeseja sair do sistema?\n1- Para NÃO\n2- Para SIM");
-					entrarMenu = sc.nextInt();
+				int indiceFilme = 0;
+				for (Filme filme : listaDeFilmes) {
+					String nomeFilme = filme.getNome();
+					String sinopse = filme.getSinopse();
+					String generoFilme = filme.getGenero();
+					indiceFilme++;
+
+					System.out.println("OPÇÃO " + (indiceFilme) + "- GENERO: " + generoFilme + ", FILME: " + nomeFilme
+							+ ", SINÓPSE: " + sinopse);
+				}
+				System.out.println(
+						"--------------------------------------------------------------------------------------------------");
+
+				// Abre lista para escolha do filme
+				List<Filme> escolhaFilmesDaLista = new ArrayList<>();
+
+				escolhaFilmesDaLista.add(exterminadorDoFuturo);
+				escolhaFilmesDaLista.add(clubeDaLuta);
+				escolhaFilmesDaLista.add(oLoboDeWallStreet);
+				escolhaFilmesDaLista.add(ameliePoulain);
+				escolhaFilmesDaLista.add(crepusculo);
+				escolhaFilmesDaLista.add(harryPotter);
+
+				boolean stopWhileNovoFilme = false;
+				int escolhaFilme = 0;
+				while (!stopWhileNovoFilme) {
+					System.out.print("Olá " + listaDeUsuarios.getNomeCompleto()
+							+ ", escolha um dos filmes para você assistir: ");
+					escolhaFilme = sc.nextInt() - 1;
 					sc.nextLine();
+					listaDeUsuarios.escolhaDoFilme(escolhaFilmesDaLista, escolhaFilme);
+					generosMaisUsuarios.generosMaisAssistidosUsuario(escolhaFilmesDaLista, cadastroUsuarios,
+							escolhaFilme, secaoUsuario);
+					generosMaisPlataforma.generosMaisAssistidosPlataforma(escolhaFilmesDaLista, cadastroUsuarios,
+							escolhaFilme, secaoUsuario);
 
-					if (entrarMenu == 2) {
-						break;
-					} else if (entrarMenu == 1) {
-						
-						generosMaisUsuarios.setContAcao(0);
-						generosMaisUsuarios.setContAventura(0);
-						generosMaisUsuarios.setContCartoon(0);
-						generosMaisUsuarios.setContDocumentario(0);
-						generosMaisUsuarios.setContDrama(0);
-						generosMaisUsuarios.setContIndependentes(0);
-						generosMaisUsuarios.setContRomance(0);
-						generosMaisUsuarios.setContSuspense(0);
-						generosMaisUsuarios.setContOutros(0);
+					System.out.println("\nDeseja escolher mais algum filme?1- SIM, 2- NÃO");
+					System.out.print("-->");
+					int maisFilmes = sc.nextInt();
 
-						break;
+					if (maisFilmes == 1) {
+						stopWhileNovoFilme = false;
+					} else if (maisFilmes == 2) {
+						stopWhileNovoFilme = true;
+						;
 					} else {
-						System.out.println("\nPor gentileza, digite uma das opções listadas\n");
+						System.out.println("\nPor gentileza, digite uma opção válida.\n");
 					}
 				}
 
-				if (entrarMenu == 1 || entrarMenu == 2) {
-					break;
-				}
-			}
+				listaDeUsuarios.likeDeslike();
 
-			secaoUsuario++;
+				// Exibe a lista de usuários, para que o usuário da secao possa escoher outros
+				// usuário para indicar filmes
+				int escolha = 0;
+				boolean usuarioRecomenda = true;
+				boolean stopWhileRecomenda = true;
+				int controleEnunciado = secaoUsuario;
+
+				int sair = 0;
+
+				while (usuarioRecomenda) {
+					while (stopWhileRecomenda) {
+						int controleLista = 0;
+
+						for (int i = controlaListaUsuarios; i < cadastroUsuarios.size(); i++) {
+							if (secaoUsuario == i) {
+								/**
+								 * Impede que o usuário selecione ele mesmo para recomendar filmes, permite que
+								 * o indice dele nao seja exibido enquanto usa o sistema
+								 */
+
+							} else {
+
+								if (controleEnunciado == secaoUsuario) {
+
+									System.out.println(
+											"----------------------------------------------------------------------------\n");
+									System.out.println(
+											"Agora precisamos que você selecione um dos usuários listados abaixo e faça uma recomendação de filme\n");
+									System.out.println("Lembrando que você só pode fazer uma recomendação por mês :)");
+								}
+								controleEnunciado++;
+
+								String nomeUsuarioLista = cadastroUsuarios.get(i).getNomeCompleto();
+								System.out.println((i + 1) + "- " + nomeUsuarioLista);
+								controleLista = 2;
+							}
+						}
+
+						// Fazer um loop com excecoes
+						if (controleLista == 2) {
+							System.out.print("-->");
+							escolha = sc.nextInt();
+
+							for (int h = 0; h < cadastroUsuarios.size(); h++) {
+								if (escolha <= cadastroUsuarios.size()) {
+									System.out.println(
+											"Você escolheu " + cadastroUsuarios.get(escolha - 1).getNomeCompleto());
+									System.out.println(
+											"----------------------------------------------------------------------------");
+
+									listaDeUsuarios.usuarioRecomenda();
+
+									stopWhileRecomenda = false;
+									break;
+								} else {
+									System.out.println(
+											"\nPor gentileza, digite um número correspondente a lista de usuários.\n");
+								}
+								break;
+							}
+						}
+						stopWhileRecomenda = false;
+					}
+					sair++;
+					if (sair == cadastroUsuarios.size()) {
+						break;
+					}
+				}
+
+				// Sugestão de filme usuário
+				Interacao sugestaoFilmeUsuarios = new Interacao();
+				sugestaoFilmeUsuarios.sugestaoDoUuario();
+
+				// Criar um método para armazenar a recomendacao
+
+				acaoUsuario = generosMaisUsuarios.getContAcao();
+				aventuraUsuario = generosMaisUsuarios.getContAventura();
+				romanceUsuario = generosMaisUsuarios.getContRomance();
+				cartoonUsuario = generosMaisUsuarios.getContCartoon();
+				dramaUsuario = generosMaisUsuarios.getContDrama();
+				documentarioUsuario = generosMaisUsuarios.getContDocumentario();
+				independentesUsuario = generosMaisUsuarios.getContIndependentes();
+				suspenseUsuario = generosMaisUsuarios.getContSuspense();
+				outrosUsuario = generosMaisUsuarios.getContOutros();
+
+				acaoPlataforma = generosMaisPlataforma.getContAcao();
+				aventuraPlataforma = generosMaisPlataforma.getContAventura();
+				romancePlataforma = generosMaisPlataforma.getContRomance();
+				cartoonPlataforma = generosMaisPlataforma.getContCartoon();
+				dramaPlataforma = generosMaisPlataforma.getContDrama();
+				documentarioPlataforma = generosMaisPlataforma.getContDocumentario();
+				independentesPlataforma = generosMaisPlataforma.getContIndependentes();
+				suspensePlataforma = generosMaisPlataforma.getContSuspense();
+				outrosPlataforma = generosMaisPlataforma.getContOutros();
+
+				SomaGenerosUsuarios comparaGenerosMaisUsuarios = new SomaGenerosUsuarios(acaoUsuario, aventuraUsuario,
+						romanceUsuario, cartoonUsuario, dramaUsuario, documentarioUsuario, independentesUsuario,
+						suspenseUsuario, outrosUsuario);
+				SomaGenerosPlataforma comparaGenerosMaisPlataforma = new SomaGenerosPlataforma(acaoPlataforma,
+						aventuraPlataforma, romancePlataforma, cartoonPlataforma, dramaPlataforma,
+						documentarioPlataforma, independentesPlataforma, suspensePlataforma, outrosPlataforma);
+
+				System.out.println("Os generos mais assistidos por " + listaDeUsuarios.getNomeCompleto() + " são:");
+				comparaGenerosMaisUsuarios.generoMaisUsuario();
+
+				System.out.println("\nOs generos mais assistidos do DevInFlix são:");
+				comparaGenerosMaisPlataforma.generoMaisPlataforma();
+
+				boolean saidaPrograma = true;
+				while (saidaPrograma) {
+					boolean stopWhile = true;
+					while (stopWhile) {
+						System.out.println("\nDeseja sair do sistema?\n1- Para NÃO\n2- Para SIM");
+						entrarMenu = sc.nextInt();
+						sc.nextLine();
+
+						if (entrarMenu == 2) {
+							break;
+						} else if (entrarMenu == 1) {
+
+							// Zera os contadores da seção do usuário
+							generosMaisUsuarios.setContAcao(0);
+							generosMaisUsuarios.setContAventura(0);
+							generosMaisUsuarios.setContCartoon(0);
+							generosMaisUsuarios.setContDocumentario(0);
+							generosMaisUsuarios.setContDrama(0);
+							generosMaisUsuarios.setContIndependentes(0);
+							generosMaisUsuarios.setContRomance(0);
+							generosMaisUsuarios.setContSuspense(0);
+							generosMaisUsuarios.setContOutros(0);
+
+							break;
+						} else {
+							System.out.println("\nPor gentileza, digite uma das opções listadas\n");
+						}
+					}
+
+					if (entrarMenu == 1 || entrarMenu == 2) {
+						break;
+					}
+				}
+
+				secaoUsuario++;
+			} else {
+				System.out.println(
+						"Parece que houve um problema com o pagamento da mensalidade, retorne ao sistema e realize o pagamento.");
+				break;
+			}
 		}
 	}
 
